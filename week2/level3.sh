0c707a8175631f6914ce065ec22ee86613091bc9
Rocky Chen <rthenc@icloud.com>
2022-07-01 17:18:02 +0000

    update level3 sh script with script that doesn't work

--- week2/level3.sh
+++ week2/level3.sh
@@ -1,3 +1,5 @@
 # obtain the 1,000 most frequent words on normalized titles
 cat /workspace/datasets/fasttext/normalized_titles.txt | tr " " "\n" | grep "...." | sort | uniq -c | sort -nr | head -1000 | grep -oE '[^ ]+$' > /workspace/datasets/fasttext/top_words.txt
 
+# index
+./index-data.sh -r -p /workspace/search_with_machine_learning_course/week2/conf/bbuy_products.json # DOES NOT WORK
